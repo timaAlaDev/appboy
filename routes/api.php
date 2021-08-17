e0daefea19c112e7b185fde6218cffc982827dce
timaAlaDev <85520365+timaAlaDev@users.noreply.github.com>
2021-08-18 01:42:58 +0600

    New Routes

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,17 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('user/register',        'Api\UserController@register');
-Route::post('user/login',           'Api\UserController@login');
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::prefix( 'v1' )->namespace( 'Api' )->group( function() {
     // PROJECT
         Route::get(  'projects',        'ProjectController@get');
-        Route::post( 'projects',        'ProjectController@create');
         Route::post( 'projects/edit ',  'ProjectController@update');
+        Route::post( 'projects',        'ProjectController@create');
         Route::get(  'projects/{id}',   'ProjectController@getById');
     // #END# PROJECT
     
@@ -22,24 +24,11 @@ Route::prefix( 'v1' )->namespace( 'Api' )->group( function() {
         Route::post('project_payment',  'ProjectPaymentController@create');
     // #END# PROJECT PAYMENT
 
-    // USER
-        Route::post('user/edit',                        'UserController@edit');
-
-        // RESTORE PASSWORD
-            Route::post('user/restore_password_email_send', 'UserController@restorePasswordEmailSend');
-            Route::post('user/restore_password_email',      'UserController@restorePasswordEmail');
-        // #END# RESTORE PASSWORD
-
-    // #END# USER
-
     // CITY
         Route::get(     'city',         'CityController@get');
         Route::post(    'city',         'CityController@create');
         Route::put(     'city/{id}',    'CityController@update');
         Route::delete(  'city/{id}',    'CityController@delete');
     // #END# CITY
-});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('verify_user', 'Api\UserController@verifyEmail')->name('verifyUser');
-
 Route::get('/{query}', function () {
     return view('app');
 })->where('query', "^((?!api).)*$");
